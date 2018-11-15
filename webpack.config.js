1f22f59763484756fb99368276f752cb616fdcc0
Agustin ORTU <agustin.ortu@MJGM.GBA.GOB.AR>
2018-11-15 12:19:33 -0300

    limpio el webpack.config y agrego el index.html al repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,12 @@
 // Configuracion
 var path = require('path');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
 var bundlesSrcPath = path.resolve(__dirname, './src/main/resources/static/dist');
-var templateSrcServerPath = path.resolve(__dirname, './src/main/resources/templates/');
 const webpack = require('webpack');
 
 module.exports = {
 	entry: './frontend-reactjs/src/js/App.jsx',
 	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: 'src/',
-		port: 8091,
-		// Send API requests on localhost to API server get around CORS.
-		proxy : {
-			'/**' : {
-				target : {
-					host : "localhost",
-					protocol : 'http:',
-					port : 8081
-				}
-			}
-		},
-		hot: true,
-		inline: true
-	},
 	plugins: [
-		new CopyWebpackPlugin([{
-			from: 'frontend-reactjs/src/index.html',
-			to: templateSrcServerPath
-		}]),
-		new webpack.HotModuleReplacementPlugin(),
 		// Hago global jQuery
 		new webpack.ProvidePlugin({
 			$: 'jquery',
